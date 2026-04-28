5795da902b5ca5190c61e729dcfbca26cb207fec
Krupa  J. Baldha <krupabld12@gmail.com>
2026-04-28 19:27:40 +0530

    Add file counting functionality
    This program counts the number of characters, words, and lines in a specified file and outputs the results.

---
+++ CES8.java/CES8_26.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+class FileCount {
+    public static void main(String args[]) {
+        int characters = 0, words = 0, lines = 0;
+
+        try {
+            FileReader fr = new FileReader(args[0]);
+            BufferedReader br = new BufferedReader(fr);
+
+            String str;
+
+            while ((str = br.readLine()) != null) {
+                lines++;
+                characters += str.length();
+
+                String arr[] = str.trim().split("\\s+");
+
+                if (!str.trim().equals("")) {
+                    words += arr.length;
+                }
+            }
+
+            br.close();
+
+            System.out.println("Number of Lines = " + lines);
+            System.out.println("Number of Words = " + words);
+            System.out.println("Number of Characters = " + characters);
+        }
+
+        catch (Exception e) {
+            System.out.println("Error: " + e);
+        }
+    }
+}
